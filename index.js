8f19c5cf73e61fd9388da35a0c5c48e174b3ea20
James Austen <aus.james@gmail.com>
2018-08-01 16:25:13 +0100

    Mapped additional endpoints

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { createApi } = require('./lib/api')
-const { createClient } = require('./lib/client')
-const { getApiKeyFromEnv } = require('./lib/config')
+const {createApi} = require('./lib/api');
+const {createClient} = require('./lib/client');
+const {getApiKeyFromEnv} = require('./lib/config');
 
 function createInstance(apiKey, opts = null) {
-  return createApi(createClient(apiKey, opts))
+    return createApi(createClient(apiKey, opts));
 }
 
 module.exports = {
-  createClient: (apiKey, opts = null) => createInstance(apiKey, opts),
-  createClientFromEnv: (opts = null) => createInstance(getApiKeyFromEnv(), opts)
-}
+    createClient: (apiKey, opts = null) => createInstance(apiKey, opts),
+    createClientFromEnv: (opts = null) => createInstance(getApiKeyFromEnv(), opts)
+};
