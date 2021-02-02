ccf230a8da8b29b96304f7ae09db312f39c34be9
James Austen <aus.james@gmail.com>
2021-02-02 12:00:14 +0000

    Add TypeScript

--- rollup.config.js
+++ rollup.config.js
@@ -2,30 +2,61 @@ import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
+import pkg from './package.json';
+
+const extensions = [
+    '.js', '.jsx', '.ts', '.tsx',
+];
+
+const name = 'geTrackerApi';
 
 export default {
-    input: 'src/index.js',
-    output: {
-        name: 'geTrackerApi',
-        file: 'dist/index.min.js',
+    input: './src/index.ts',
+    // output: {
+    //     name: 'geTrackerApi',
+    //     file: 'dist/index.min.js',
+    //     format: 'cjs',
+    // },
+    output: [{
+        file: pkg.main,
         format: 'cjs',
-    },
+    }, {
+        file: pkg.module,
+        format: 'es',
+    }, {
+        file: pkg.browser,
+        format: 'iife',
+        name,
+
+        // https://rollupjs.org/guide/en/#outputglobals
+        globals: {},
+    }],
+
+    // Do not include these modules in the bundle
     external: ['axios', 'moment', 'deepmerge'],
+
     plugins: [
-        nodeResolve({
-            jsnext: true,
-            main: true,
-            browser: true,
-        }),
+        // Allows node_modules resolution
+        nodeResolve({extensions}),
+
+        // Bundle CJS modules
         commonjs(),
+
+        // typescript({
+        //     tsconfig: './tsconfig.json'
+        // }),
+
+        // Compile TypeScript/JavaScript
         babel({
-            exclude: 'node_modules/**',
-            plugins: [],
+            extensions,
             babelHelpers: 'bundled',
+            include: ['src/**/*'],
+            exclude: './node_modules/**'
         }),
-        replace({
-            exclude: 'node_modules/**',
-            ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-        }),
+
+        // replace({
+        //     exclude: './node_modules/**',
+        //     ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+        // }),
     ],
 };
