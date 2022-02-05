c7f5730aa2d31a95666dbfe4581265756ba4b744
James Austen <aus.james@gmail.com>
2022-02-05 09:31:14 +0000

    Add user types

--- src/lib/endpoints/UserWrapper.ts
+++ src/lib/endpoints/UserWrapper.ts
@@ -1,17 +1,47 @@
 import APIBaseWrapper from './APIBaseWrapper';
 
+export type UserPremium = {
+    premium: boolean;
+    trial: boolean;
+    view: boolean;
+};
+
+export type UserPermissions = {
+    moderator: string;
+    staff: string;
+    admin: string;
+    disabled: string;
+}
+
+export type User = {
+    userId: string;
+    name: string;
+    email: string;
+    totalProfit: number;
+    registeredOn: Date;
+    emailVerified: boolean;
+    profilePic: string;
+    slug: string;
+    apiKey: string;
+    url: string;
+    class: string;
+    premium: UserPremium;
+    permissions: UserPermissions;
+    avatars: Array<string>;
+};
+
 export default class UserWrapper extends APIBaseWrapper {
-    getById(userId: string) {
+    getById(userId: string): Promise<Array<User>> {
         return this._wrapGet(`users/${userId}`);
     }
 
-    search(query: string) {
+    search(query: string): Promise<Array<User>> {
         return this._wrapPost(`users/search`, {
             query
         });
     }
 
-    getAuthenticateUser() {
+    getAuthenticateUser(): Promise<User> {
         return this._wrapGet('users/me');
     }
 }
