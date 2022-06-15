253d6f80922567b47675490b4dc7c8359d6fc442
James Austen <aus.james@gmail.com>
2022-06-15 13:57:20 +0100

    Bump version to v5.0.1

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -23,8 +23,8 @@ const DefaultOptions: AxiosRequestConfig = {
     // specify a custom user-agent header to identify the client used
     // and for a point of contact
     headers: {
-        'User-Agent': 'ge-tracker-api client v5.0.0',
-        'X-Api-Client': 'ge-tracker-api client v5.0.0',
+        'User-Agent': 'ge-tracker-api client v5.0.1',
+        'X-Api-Client': 'ge-tracker-api client v5.0.1',
         Accept: `application/x.getracker.${GE_TRACKER_API_VERSION}+json`,
     },
 };
