71a3a5d10cae3df53e7a95bde331f3d87a4d8a13
James Austen <aus.james@gmail.com>
2023-01-25 08:47:14 +0000

    Bump version to v6.0.0-11

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -28,8 +28,8 @@ const DefaultOptions: CreateAxiosDefaults = {
     // specify a custom user-agent header to identify the client used
     // and for a point of contact
     headers: {
-        'User-Agent': 'ge-tracker-api client v6.0.0-10',
-        'X-Api-Client': 'ge-tracker-api client v6.0.0-10',
+        'User-Agent': 'ge-tracker-api client v6.0.0-11',
+        'X-Api-Client': 'ge-tracker-api client v6.0.0-11',
         Accept: `application/x.getracker.${GE_TRACKER_API_VERSION}+json`,
     },
 };
