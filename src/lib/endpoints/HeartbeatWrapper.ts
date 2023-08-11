f74520e4e3055fdca148f18adce674a471a346ee
James Austen <aus.james@gmail.com>
2023-08-11 13:32:12 +0100

    Add user and guest heartbeats

--- src/lib/endpoints/HeartbeatWrapper.ts
+++ src/lib/endpoints/HeartbeatWrapper.ts
@@ -4,4 +4,12 @@ export default class HeartbeatWrapper extends APIBaseWrapper {
     getHeartbeat() {
         return this._wrapGet('heartbeat');
     }
+
+    user() {
+        return this.client.post('heartbeat');
+    }
+
+    guest() {
+        return this.client.post('heartbeat/guest');
+    }
 }
