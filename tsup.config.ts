ff44157e10ce875565811489acdd59f5b98dbb92
James Austen <aus.james@gmail.com>
2023-09-19 13:51:19 +0100

    Refactor microbundle to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, type Options } from 'tsup';
+
+const cfg: Options = {
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    treeshake: false,
+    dts: true,
+    format: ['esm', 'cjs'],
+};
+
+export default defineConfig([
+    {
+        ...cfg,
+        entry: {
+            index: 'src/index.ts',
+        },
+        outDir: 'dist',
+    },
+]);
