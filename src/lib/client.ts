32b1070486d1044847051d467c71dcc24153bbaf
James Austen <aus.james@gmail.com>
2023-10-09 11:24:47 +0100

    Bump version to v6.2.2

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -27,7 +27,7 @@ const DefaultOptions: CreateAxiosDefaults = {
 
   // set api client headers
   headers: {
-    'X-Api-Client': 'ge-tracker-api client v6.2.1',
+    'X-Api-Client': 'ge-tracker-api client v6.2.2',
     Accept: `application/x.getracker.${GE_TRACKER_API_VERSION}+json`,
   },
 };
